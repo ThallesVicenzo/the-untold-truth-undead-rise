489fe32a94840a918a4730c2c4acb98cf06ebf2e
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-03-03 01:02:51 -0300

    Destructible doors added

---
+++ Assets/Game Folder/Decoration/Script/DoorHit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHit : MonoBehaviour
+{
+    int lifeChange;
+    public Transform lifeBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeChange = GetComponent<Character>().life;
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lifeChange != GetComponent<Character>().life )
+        {
+            lifeChange = GetComponent<Character>().life;
+            GetComponent<Character>().skin.GetComponent<Animator>().Play("DoorHit");
+        }
+
+        if(GetComponent<Character>().life <= 0)
+        {
+            Destroy(transform.gameObject);
+        }
+
+        lifeBar.localScale = new Vector3((1 * GetComponent<Character>().life) / 10f, 1, 1);
+    }
+}
