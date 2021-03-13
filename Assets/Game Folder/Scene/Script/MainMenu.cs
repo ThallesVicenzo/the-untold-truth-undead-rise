f28e9eec61d012ccb0b1c1ba9c6f6a134eb0b1f7
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-03-13 17:19:14 -0300

    Main Menu added

---
+++ Assets/Game Folder/Scene/Script/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Level0");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
